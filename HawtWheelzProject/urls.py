32e239366bc2d255b426804c848c4917d1c009da
ezequielh97 <36419172+ezequielh97@users.noreply.github.com>
2022-02-16 15:52:44 -0800

    wheelz got replaced by the home app. home will be the homepage of the website.

--- HawtWheelzProject/urls.py
+++ HawtWheelzProject/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
+    path('admin/', admin.site.urls),
+
 ]
