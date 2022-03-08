dc490e448607c4ad6e4d524b84bd96a36b0a0df0
ezequielh <>
2022-03-07 16:24:05 -0800

    I changed how the static files are refrenced

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Car
 
 
 def homepage(request):
-    return render(request, 'home/index.html', {})
+    new_cars = Car.objects.order_by('-date_added')[:5]
+    data = {'new_cars': new_cars
+            }
+
+    return render(request, 'home/index.html', data)
