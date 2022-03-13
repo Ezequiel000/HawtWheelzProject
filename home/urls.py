612ba2fc6b3005dfc119da3dbd081c0adb974e77
ezequielh <>
2022-03-13 16:28:47 -0700

    added started inventory.html, its view ,and url

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.homepage, name='homepage')
+   path('', views.homepage, name='homepage'),
+   path('inventory/', views.inventory, name='inventory')
    ]
