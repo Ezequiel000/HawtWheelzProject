1c5e59327de037defef6fc704708436600ad18e2
Stacey <stacey.munoz@ymail.com>
2022-03-13 19:19:30 -0700

    added search bar no results yet

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.homepage, name='homepage')
+   path('', views.homepage, name='homepage'),
+   path('search_results', views.search_results, name='search_results' )
    ]
--- home/views.py
+++ home/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .models import Car
 
 
+
 def homepage(request):
     new_cars = Car.objects.order_by('-date_added')[:5]
     data = {'new_cars': new_cars
             }
 
     return render(request, 'home/index.html', data)
+
+def search_results(request):
+    if request.method == "POST":
+        searched = request.POST.get('searched', False);
+        cars = Car.objects.filter(name__contains=searched)
+        return render(request, 'home/search_results.html', {'searched':searched, 'cars':cars})
+    else:
+        return render(request, 'home/search_results.html', {})
