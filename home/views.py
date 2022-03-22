d1640464c0a916bb0ddd7f7d46393b28374d7878
Tricepticon <007377845@coyote.csusb.edu>
2022-03-22 00:19:55 -0700

    -Created an about page -Created the views and urls so that the navbar can link the titles to their page. -Small logo onto the navbar

--- home/views.py
+++ home/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
 
-
 def homepage(request):
     new_cars = Car.objects.order_by('-date_added')[:5]
-    data = {'new_cars': new_cars
-            }
+    data = {'new_cars': new_cars}
 
     return render(request, 'home/index.html', data)
 
@@ -13,3 +11,7 @@ def homepage(request):
 def inventory(request):
 
     return render(request, 'home/inventory.html')
+
+def about(request):
+
+    return render(request, 'home/about.html')
