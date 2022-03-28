16a13974934d1be55ca4d985d9b2cf17466f8ec7
ezequielh <>
2022-03-27 21:50:43 -0700

    Made migration 0005_delete_navbar_car_color_car_year_and_more.py

--- home/models.py
+++ home/models.py
@@ -1,6 +1,5 @@
 import datetime
 from django.db import models
-from django.utils.html import mark_safe
 from django.utils import timezone
 
 
@@ -8,7 +7,7 @@ class Car(models.Model):
     # Fields
     name = models.CharField(max_length=20)
     make = models.CharField(max_length=20)
-    date_added = models.DateTimeField('date added', default=timezone.now())
+    date_added = models.DateTimeField('date added')
     model = models.CharField(max_length=20)
     color = models.CharField(max_length=20, default='color')
     year = models.IntegerField(default=0)
@@ -22,12 +21,3 @@ class Car(models.Model):
     def was_added_recently(self):  # will return true if the car was added in the last week
         now = timezone.now()
         return now - datetime.timedelta(days=7) <= self.date_added <= now
-
-
-class NavBar(models.Model):
-    # Fields
-    option_text = models.CharField(max_length=20, default='Text')
-
-    # Methods
-    def __str__(self):
-        return self.option_text
