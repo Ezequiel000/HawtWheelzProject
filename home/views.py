9a5d9fe22edb5cf4b6ee6a62f3ad4db69ec8bce3
Ezequiel000 <99154705+Ezequiel000@users.noreply.github.com>
2022-03-31 18:40:18 -0700

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
 
+
 def homepage(request):
     new_cars = Car.objects.order_by('-date_added')[:5]
-    data = {'new_cars': new_cars}
+    data = {'new_cars': new_cars
+            }
 
     return render(request, 'home/index.html', data)
 
 
 def inventory(request):
+    #this holds all cars in the database 
+    inventory_cars = Car.objects.all()
 
-    return render(request, 'home/inventory.html')
+    context = {
+        'inventory_cars': inventory_cars
+    }
+    return render(request, 'home/inventory.html', context)#context contains all data sent
 
 def about(request):
 
