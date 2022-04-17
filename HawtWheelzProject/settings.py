8f6478711cdb094cebd483d83c51c39fef17255d
ezequielh <>
2022-04-16 20:14:59 -0700

    added Bucketeer config variables

--- HawtWheelzProject/settings.py
+++ HawtWheelzProject/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import dj_database_url
+import boto3
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
