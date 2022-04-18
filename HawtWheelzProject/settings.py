7ed9e34f0ed95d65a80fafcb11d6a0325a944d50
ezequielh <>
2022-04-18 14:17:12 -0700

    added bucketeer to req

--- HawtWheelzProject/settings.py
+++ HawtWheelzProject/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import boto3
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,4 +129,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_ACCESS_KEY_ID = 'BUCKETEER_AWS_ACCESS_KEY_ID'
 AWS_S3_SECRET_ACCESS_KEY = 'BUCKETEER_AWS_SECRET_ACCESS_KEY'
 AWS_S3_STORAGE_BUCKET_NAME = 'BUCKETEER_BUCKET_NAME'
-AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_REGION_NAME = 'BUCKETEER_AWS_REGION'
