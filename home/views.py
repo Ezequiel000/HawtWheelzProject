13359ee350caf30a22624f143d3e47557f99fc5c
Osman Arellano <006954305@coyote.csusb.edu>
2022-04-21 18:40:12 -0700

    Added a slider in the home page, so now we should be able to slide cards
    Started on the about us page just have text and an image placeholder need to think of what to add.

--- home/views.py
+++ home/views.py
@@ -4,7 +4,9 @@ from .models import Car
 
 def homepage(request):
     new_cars = Car.objects.order_by('-date_added')[:5]
-    data = {'new_cars': new_cars
+    best_deal = Car.objects.order_by('price')[:6]
+    data = {'new_cars': new_cars,
+            'best_deal': best_deal,
             }
 
     return render(request, 'home/index.html', data)
