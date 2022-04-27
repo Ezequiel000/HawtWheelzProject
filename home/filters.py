405abdd1025e3f85bc9f6b2625230fb95477a0ec
Stacey <stacey.munoz@ymail.com>
2022-04-27 13:21:10 -0700

    working and tested filters and search bar

--- home/filters.py
+++ home/filters.py
@@ -3,7 +3,7 @@ from .models import Car
 
 
 class CarFilter(django_filters.FilterSet):
-     name = django_filters.CharFilter(field_name="name", lookup_expr='icontains')
+     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
      price_gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
      price_lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
      class Meta:
